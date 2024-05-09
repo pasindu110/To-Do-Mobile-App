b48c70fa6f0a41e30254279e19f69eb68e1a699d
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-09 19:01:37 +0530

    Edit

--- app/src/main/java/com/example/a2ndattempt/AddNoteActivity.kt
+++ app/src/main/java/com/example/a2ndattempt/AddNoteActivity.kt
@@ -2,10 +2,23 @@ package com.example.a2ndattempt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import com.example.a2ndattempt.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db : NoteDatabaseHelper
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_add_note)
+
+        db = NoteDatabaseHelper(this)
+
+        binding
     }
 }
--- app/src/main/java/com/example/a2ndattempt/MainActivity.kt
+++ app/src/main/java/com/example/a2ndattempt/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.a2ndattempt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a2ndattempt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.imageButton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
