cf97447ac56e6aebc70ac9cde7552876658aef48
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-10 17:40:03 +0530

    Add editDB

--- app/src/main/java/com/example/a2ndattempt/NoteDatabaseHelper.kt
+++ app/src/main/java/com/example/a2ndattempt/NoteDatabaseHelper.kt
@@ -85,4 +85,12 @@ class NoteDatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_N
 
     }
 
+    fun deleteNote(noteId: Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+    }
+
 }
