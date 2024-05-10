e08411c9a1c08be90fbde4a68626ed5c72b77117
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-10 17:29:23 +0530

    Update update part 2

--- app/src/main/java/com/example/a2ndattempt/UpdateNoteActivity.kt
+++ app/src/main/java/com/example/a2ndattempt/UpdateNoteActivity.kt
@@ -3,17 +3,17 @@ package com.example.a2ndattempt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.a2ndattempt.databinding.ActivityUpdateBinding
+import com.example.a2ndattempt.databinding.ActivityUpdateNoteBinding
 
 class UpdateNoteActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var binding: ActivityUpdateNoteBinding
     private lateinit var db:NoteDatabaseHelper
     private  var noteId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUpdateBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_update)
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_update_note)
 
         db = NoteDatabaseHelper(this)
 
@@ -25,11 +25,11 @@ class UpdateNoteActivity : AppCompatActivity() {
 
         val note = db.getNoteByID(noteId)
         binding.updatetitleEditText.setText(note.title)
-        binding.updatecontentEditText.setText(note.content)
+        binding.updatecontent.setText(note.content)
 
-        binding.updatesaveButton.setOnClickListener {
+        binding.updateButton.setOnClickListener {
             val newTitle = binding.updatetitleEditText.text.toString()
-            val  newContent = binding.updatecontentEditText.text.toString()
+            val  newContent = binding.updatecontent.text.toString()
             val updateNote = Note(noteId,newTitle,newContent)
             db.updateNote(updateNote)
             finish()
