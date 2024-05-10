2c952cad654596c50626a02386039d907cb6f3ce
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-10 15:54:10 +0530

    Edit main 3

--- app/src/main/java/com/example/a2ndattempt/MainActivity.kt
+++ app/src/main/java/com/example/a2ndattempt/MainActivity.kt
@@ -3,15 +3,27 @@ package com.example.a2ndattempt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a2ndattempt.databinding.ActivityMainBinding
+import com.example.to_dolistapp.NoteAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db : NoteDatabaseHelper
+    private lateinit var notesAdapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        db = NoteDatabaseHelper(this)
+        notesAdapter = NoteAdapter(db.getAllNotes(),this)
+
+        binding.noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.noteRecyclerView.adapter = notesAdapter
+
 
         binding.addButton.setOnClickListener{
             val intent = Intent(this,AddNoteActivity::class.java)
@@ -20,4 +32,9 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
+    }
+
 }
