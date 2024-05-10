de1701f65c37c916ee6bcc7e2a5195145c5c96af
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-10 15:54:39 +0530

    N adapter edit 3

--- app/src/main/java/com/example/a2ndattempt/NoteAdapter.kt
+++ app/src/main/java/com/example/a2ndattempt/NoteAdapter.kt
@@ -1,4 +1,49 @@
-package com.example.a2ndattempt
+package com.example.to_dolistapp
 
-class NoteAdapter {
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a2ndattempt.Note
+import com.example.a2ndattempt.NoteDatabaseHelper
+import com.example.a2ndattempt.R
+
+class NoteAdapter(private var notes: List<Note>,context: Context) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+    private val db : NoteDatabaseHelper = NoteDatabaseHelper(context)
+
+    class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val titleTextView:  TextView = itemView.findViewById(R.id.titleTextView)
+        val contextTextView:  TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton:  ImageView= itemView.findViewById(R.id.updateButton)
+        val deleteButton:  ImageView= itemView.findViewById(R.id.deleteButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item,parent,false)
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val todo = notes[position]
+        holder.titleTextView.text = todo.title
+        holder.contextTextView.text = todo.content
+
+
+    }
+    fun refreshData(newNote: List<Note>){
+        notes = newNote
+        notifyDataSetChanged()
+
+    }
+
 }
