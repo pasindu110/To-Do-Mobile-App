6bd69ac7e1990bcbb16c0369ef042b9ddf708a79
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-10 14:42:52 +0530

    edit add note

--- app/src/main/java/com/example/a2ndattempt/AddNoteActivity.kt
+++ app/src/main/java/com/example/a2ndattempt/AddNoteActivity.kt
@@ -3,6 +3,7 @@ package com.example.a2ndattempt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
+import android.widget.Toast
 import com.example.a2ndattempt.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
@@ -15,12 +16,18 @@ class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_add_note)
+        setContentView(binding.root)
 
         db = NoteDatabaseHelper(this)
 
         binding.saveButton.setOnClickListener{
-            val title = binding
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0,title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
