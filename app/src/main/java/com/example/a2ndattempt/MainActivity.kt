e1df40f723e82b0c580649fde1092bd1d2ed4aca
Pasindu Sandeep <151598463+pasindu110@users.noreply.github.com>
2024-05-12 12:36:06 +0530

    Update main Act

--- app/src/main/java/com/example/a2ndattempt/MainActivity.kt
+++ app/src/main/java/com/example/a2ndattempt/MainActivity.kt
@@ -5,36 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a2ndattempt.databinding.ActivityMainBinding
-import com.example.to_dolistapp.NoteAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var db : NoteDatabaseHelper
-    private lateinit var notesAdapter: NoteAdapter
+    private lateinit var noteAdapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         db = NoteDatabaseHelper(this)
-        notesAdapter = NoteAdapter(db.getAllNotes(),this)
+        noteAdapter = NoteAdapter(db.getAllNotes(),this)
 
         binding.noteRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.noteRecyclerView.adapter = notesAdapter
-
+        binding.noteRecyclerView.adapter = noteAdapter
 
         binding.addButton.setOnClickListener{
-            val intent = Intent(this,AddNoteActivity::class.java)
+            val  intent = Intent(this,AddNoteActivity::class.java)
             startActivity(intent)
         }
-
-
     }
+
     override fun onResume() {
         super.onResume()
-        notesAdapter.refreshData(db.getAllNotes())
+        noteAdapter.refreshData(db.getAllNotes())
     }
-
 }
